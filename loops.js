6b6667a682c2552387ed48bfd68ca22f8ec8f9e0
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-05-23 14:32:10 +0200

    ex1 loops

--- loops.js
+++ loops.js
@@ -1,2 +1,28 @@
 //Exercise 1
 //Write an algorithms which receives an integers n and prints:
+
+for (let n=1; n <=12; n++ ) {
+    console.log (n)
+}
+
+
+//Descending
+
+let i
+for(let i=10; i>0; i--){
+  console.log(i)
+}
+
+//-n  to n
+
+let n 
+for (let n=-10; n <=10; n++) {
+    console.log (n)
+}
+
+// odd numbers 1 to n
+
+let a
+for (let a=1; a<=9; a+= 2) {
+    console.log (a)
+}
